a522acb30c174cee236c5326801df75e2919db31
Tucker Giovo <segrified@gmail.com>
2023-04-26 17:43:05 -0600

    made a program with functions to ask a name and number, then return that name and the square of the number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,37 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName(){
+        Console.Write("Please enter your name: ");
+        string input = Console.ReadLine();
+        return input;
+    }
+
+    static int PromptUserNumber(){
+        Console.Write("Please enter your favorite number: ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+        return number;
+    }
+
+    static int SquareNumber(int number){
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int sqNumber){
+        Console.WriteLine(name + ", the square of your number is " + sqNumber);
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int sqNumber = SquareNumber(number);
+        DisplayResult(name, sqNumber);
     }
 }
