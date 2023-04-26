69f462d7aa7a6f1df0b325f2936904faaf20eae5
Tucker Giovo <segrified@gmail.com>
2023-04-26 17:27:10 -0600

    made a list making program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number;
+        float total = 0;
+        int largest = 0;
+        List<int> numberList = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do {
+            Console.Write("Enter Number: ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+
+            if (number != 0){
+                numberList.Add(number);
+            }
+        } while (number != 0);
+
+        for (int i = 0; i < numberList.Count; i++){
+            total += numberList[i];
+
+            if (largest < numberList[i]){
+                largest = numberList[i];
+            }
+        }
+
+        Console.WriteLine("The sum is: " + total);
+        Console.WriteLine("The average is: " + (total / numberList.Count));
+        Console.WriteLine("The largest number is: " + largest);
     }
 }
