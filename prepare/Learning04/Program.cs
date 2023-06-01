bb7b5fd8009797b737a290aa6eb23f8a81b68921
Segrified <segrified@gmail.com>
2023-05-31 20:48:31 -0600

    Added the homework tracking program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignment1 = new MathAssignment("Tucker Giovo", "Pi Calculations", "3", "1-4");
+        WritingAssignment assignment2 = new WritingAssignment("Tucker Giovo", "Programming", "The History of This Program");
+
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment1.GetHomeworkList());
+
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
     }
 }
