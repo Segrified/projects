e1a81d6d2cadaee93c3da823c874bd99cc2c064e
Segrified <segrified@gmail.com>
2023-06-17 10:45:34 -0600

    Added the Learning05 program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> list = new List<Shape>();
+
+        Square square = new Square("red", 5);
+        Rectangle rectangle = new Rectangle("blue", 3.2, 7);
+        Circle circle = new Circle("purple", 9.9);
+
+        list.Add(square);
+        list.Add(rectangle);
+        list.Add(circle);
+
+        for (int i = 0; i < list.Count; i++) {
+            Console.WriteLine("This is a {0} {1} with an area of {2}.", list[i].GetColor(), list[i].GetType(), list[i].GetArea());
+        }
     }
 }
